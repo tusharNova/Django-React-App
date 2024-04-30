b3e3cfbcbc8b7de8fc3fcb55104d9f67d26cb85c
Tushar Mankar <tusharmankar02@gmail.com>
2024-04-30 20:29:25 +0530

    add Api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,16 @@
 import React from "react"
+import { BrowserRouter , Route, Router ,Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/home"
+import Notfound from "./pages/Notfound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Await, Naviagte } from "react-router-dom"
+import { jwtDecode } from "jwt-decode"
+import api from '../api'
+import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants'
+import { useState , useEffect} from 'react'
+
+function ProtectedRoute({ children }) {
+
+    const [isAutherized, setIsAutherized] = useState(null);
+
+    useEffect(() =>{
+        auth.catch(() => setIsAutherized(false))
+    }, [])
+
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try {
+            const res = await api.post("/api/toekn/refresh", { refresh: refreshToken });
+            if(res.status == 200){
+                localStorage.setItem(ACCESS_TOKEN , res.data.access)
+                setIsAutherized(true)
+            }
+            else{
+                setIsAutherized(false)
+            }
+        } catch (error) {
+            console.log(error);
+            setIsAutherized(false)
+        }
+    }
+
+    const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            setIsAutherized(false)
+            return
+        }
+        const decode = jwtDecode(token)
+        const tokenExpriation = decode.exp
+        const now = Date.now() / 100
+
+        if (tokenExpriation < now) {
+            await refreshToken()
+        } else {
+            setIsAutherized(true)
+        }
+    }
+
+    if (isAutherized == null) {
+        return <div>Loading....</div>
+    }
+    return isAutherized ? children : <Naviagte to="/login" />
+
+}
+
+export default ProtectedRoute
